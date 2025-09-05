63d047fdb70fe1ca7e5fe26b9078d5b39c98d710
John Gulbronson <john@benzinga.com>
2025-09-05 19:52:09 -0400

    open october

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,7 +1,8 @@
 import 'styled-components';
-import { Theme } from './theme';
+import type { Theme } from './theme';
 
 // Extend the DefaultTheme interface to include our custom theme
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends Theme {}
 }
